ed13ee1e6438c64ba778188b7004c4c9d02094dd
elijahr <elijahr@users.noreply.github.com>
2024-09-07 01:32:08 -0500

    Fix widget

--- apps/glucowidget/glucowidget/index.ts
+++ apps/glucowidget/glucowidget/index.ts
@@ -8,7 +8,21 @@ import {
 // global["WIDGETS"] = {}; // <-- for development only
 
 (function () {
-  let data = {};
+  let data: GlucoseData = {
+    a: "",
+    ar: "→",
+    d: "",
+    dl: "",
+    g: "120",
+    o: "",
+    r: "",
+    rl: "",
+    rv: "",
+    s: "",
+    t: "",
+    td: "",
+    u: "",
+  };
 
   onData((d: GlucoseData) => {
     data = d;
@@ -18,12 +32,12 @@ import {
     // @ts-expect-error `g` is provided by Bangle.js APIs
     g.reset(); // reset the graphics context to defaults (color/font/etc)
     // @ts-expect-error `this` is configured by Bangle.js APIs
-    g.drawString(data.g, this.x, this.y);
+    g.drawString(data.g + data.ar, this.x, this.y);
   }
 
   // @ts-expect-error `WIDGETS` is provided by Bangle.js APIs
   WIDGETS.glucowidget = {
-    area: "bl", // tl (top left), tr (top right), bl (bottom left), br (bottom right), be aware that not all apps support widgets at the bottom of the screen
+    area: "tl", // tl (top left), tr (top right), bl (bottom left), br (bottom right), be aware that not all apps support widgets at the bottom of the screen
     width: 28, // how wide is the widget? You can change this and call Bangle.drawWidgets() to re-layout
     draw // called to draw the widget
   };
